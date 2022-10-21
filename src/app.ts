526d693605657fbfe853614bf3000abf925f7356
jakubserwin <kuba.serwin99@gmail.com>
2022-10-21 14:40:01 +0200

    project architecture

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-// import express, { Request, Response } from "express"
-const express = require('express')
+import express, { Request, Response } from 'express'
 
 const app = express()
 const port = 5000
 
 app.get('/', (_: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
