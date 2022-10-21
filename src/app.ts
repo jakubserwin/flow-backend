9ec8e5f25259c31f5edc85a8a7f523e3ed7583a6
jakubserwin <kuba.serwin99@gmail.com>
2022-10-21 16:56:25 +0200

    connect with mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
-import * as dotenv from 'dotenv'
 import express, { Request, Response } from 'express'
+import mongoose from 'mongoose'
 import config from './config'
 
-dotenv.config()
-
+// Create Express App
 const app = express()
+
+// Global Middleware
 
 app.get('/', (_: Request, res: Response) => {
   res.send('Hello World!')
 })
 
+// DB Connection
+mongoose.connect(config.databaseUrl, error => {
+  if (error != null) {
+    console.log(error)
+  } else {
+    console.info('Connection with database established!')
+  }
+})
+
 app.listen(config.port, () => {
-  console.log(`Example app listening on port ${config.port}`)
+  console.log(`Example app listening on port ${config.port}!`)
 })
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
+import * as dotenv from 'dotenv'
+
+// Read config
+dotenv.config()
+
 const config = {
-  port: process.env.PORT ?? 8000
+  port: process.env.PORT ?? 8000,
+  databaseUrl: process.env.DATABASE_URL ?? ''
 }
 
 export default config
