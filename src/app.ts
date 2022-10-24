054b1293b0939be5dd4887f4dd6c7688352e25fa
jakubserwin <kuba.serwin99@gmail.com>
2022-10-24 11:25:16 +0200

    cors middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import helmet from 'helmet'
 import rateLimit from 'express-rate-limit'
+import cors from 'cors'
 import config from './config'
 import { userRouter } from './routes'
 
@@ -16,6 +17,7 @@ app.use(rateLimit({
   message: 'Too many requests from this IP!'
 }))
 app.use(express.json())
+app.use(cors())
 
 // Routes
 app.use('/users', userRouter)
