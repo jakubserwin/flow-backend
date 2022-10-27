264a32aec5920cda5dbb430a1d0eb4bd2edf3c5d
jakubserwin <kuba.serwin99@gmail.com>
2022-10-27 09:56:57 +0200

    project controller

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -16,3 +16,55 @@ export const createProject = (req: Request, res: Response): void => {
       })
     })
 }
+
+export const getProjectsByUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const projects = Project.find({ owner: req.params.id })
+    // const guestProjects = Project.find({ members: req.params.id })
+
+    res.status(200).json({
+      status: 'Success',
+      projects
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      status: 'Failure',
+      message: 'Provided user doesn\'t have any projects!'
+    })
+  }
+}
+
+export const updateProject = (req: Request, res: Response): void => {
+  Project.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  })
+    .then(response => {
+      res.status(201).json({
+        status: 'Success',
+        project: response
+      })
+    })
+    .catch(() => {
+      res.status(400).json({
+        status: 'Failure',
+        message: 'Something went wrong while trying to update project!'
+      })
+    })
+}
+
+export const deleteProject = (req: Request, res: Response): void => {
+  Project.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).json({
+        status: 'Success',
+        project: null
+      })
+    })
+    .catch(() => {
+      res.status(400).json({
+        status: 'Failure',
+        message: 'Something went wrong while trying to delete project!'
+      })
+    })
+}
