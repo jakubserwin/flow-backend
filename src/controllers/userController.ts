9eba42e31d791fad484cc3ad55373040bacefbc3
jakubserwin <kuba.serwin99@gmail.com>
2022-11-09 07:56:32 +0100

    add types for sharp

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -37,12 +37,11 @@ export const resizeAvatar = async (req: Request, res: Response, next: NextFuncti
   if (req.file === undefined) return next()
 
   req.file.filename = `user-${req.params.id}-${Date.now()}.jpeg`
-
   await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`public/${req.file.filename}`)
+    .toFile(`src/public/${req.file.filename}`)
 
   next()
 }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import { login, protect, signUp } from '../controllers'
-import { updateUser, uploadAvatar } from '../controllers/userController'
+import { resizeAvatar, updateUser, uploadAvatar } from '../controllers/userController'
 
 const userRouter = express.Router()
 
 userRouter.post('/signup', signUp)
 userRouter.post('/login', login)
-userRouter.patch('/:id', protect, uploadAvatar, updateUser)
+userRouter.patch('/:id', protect, uploadAvatar, resizeAvatar, updateUser)
 
 export { userRouter }
