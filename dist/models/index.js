6b43e38f3d93580db69dc4508ffeb0a475e52942
jakubserwin <kuba.serwin99@gmail.com>
2022-12-23 11:31:13 +0100

    clean up

--- dist/models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _createStarExport(obj) { Object.keys(obj) .filter((key) => key !== "default" && key !== "__esModule") .forEach((key) => { if (exports.hasOwnProperty(key)) { return; } Object.defineProperty(exports, key, {enumerable: true, configurable: true, get: () => obj[key]}); }); }var _userModel = require('./userModel'); _createStarExport(_userModel);
-var _projectModel = require('./projectModel'); _createStarExport(_projectModel);
-var _boardModel = require('./boardModel'); _createStarExport(_boardModel);
-var _listModel = require('./listModel'); _createStarExport(_listModel);
-var _cardModel = require('./cardModel'); _createStarExport(_cardModel);
--- dist/routes/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _createStarExport(obj) { Object.keys(obj) .filter((key) => key !== "default" && key !== "__esModule") .forEach((key) => { if (exports.hasOwnProperty(key)) { return; } Object.defineProperty(exports, key, {enumerable: true, configurable: true, get: () => obj[key]}); }); }var _userRoutes = require('./userRoutes'); _createStarExport(_userRoutes);
-var _projectRoutes = require('./projectRoutes'); _createStarExport(_projectRoutes);
-var _boardRoutes = require('./boardRoutes'); _createStarExport(_boardRoutes);
-var _listRoutes = require('./listRoutes'); _createStarExport(_listRoutes);
-var _cardRoutes = require('./cardRoutes'); _createStarExport(_cardRoutes);
